7cc303d6c05f1d542e82e2a54492645142a9624a
Akshay Salunkhe <akshay.salunkhe705@gmail.com>
2020-11-16 20:06:48 +0530

    Test Two

--- lib/views/base_view.dart
+++ lib/views/base_view.dart
@@ -2,48 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:goldsilverrate/injector.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-class BaseView<T extends Model> extends StatefulWidget {
-  final ScopedModelDescendantBuilder<T> _builder;
-  final Function(T) onModelReady;
+// class BaseView<T extends Model> extends StatefulWidget {
+//   final ScopedModelDescendantBuilder<T> _builder;
+//   final Function(T) onModelReady;
 
-  BaseView({ScopedModelDescendantBuilder<T> builder, this.onModelReady})
-      : _builder = builder;
+//   BaseView({ScopedModelDescendantBuilder<T> builder, this.onModelReady})
+//       : _builder = builder;
 
-  @override
-  _BaseViewState<T> createState() => _BaseViewState<T>();
-}
+//   @override
+//   _BaseViewState<T> createState() => _BaseViewState<T>();
+// }
 
-class _BaseViewState<T extends Model> extends State<BaseView<T>> {
-  T _model = locator<T>();
+// class _BaseViewState<T extends Model> extends State<BaseView<T>> {
+//   T _model = locator<T>();
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    if (widget.onModelReady != null) {
-      widget.onModelReady(_model);
-    }
-    super.initState();
-  }
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     if (widget.onModelReady != null) {
+//       widget.onModelReady(_model);
+//     }
+//     super.initState();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return ScopedModel<T>(
-      model: _model,
-      child: ScopedModelDescendant<T>(
-        builder: widget._builder,
-      ),
-    );
-  }
-}
-
-// class BaseView<T extends Model> extends StatelessWidget {
 //   @override
 //   Widget build(BuildContext context) {
 //     return ScopedModel<T>(
-//       model: locator<T>(),
+//       model: _model,
 //       child: ScopedModelDescendant<T>(
-//         builder: _builder,
+//         builder: widget._builder,
 //       ),
 //     );
 //   }
 // }
+
+class BaseView<T extends Model> extends StatelessWidget {
+  final ScopedModelDescendantBuilder<T> _builder;
+  BaseView({ScopedModelDescendantBuilder<T> builder}) : _builder = builder;
+  @override
+  Widget build(BuildContext context) {
+    return ScopedModel<T>(
+      model: locator<T>(),
+      child: ScopedModelDescendant<T>(
+        builder: _builder,
+      ),
+    );
+  }
+}
